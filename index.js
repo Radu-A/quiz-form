1aeb60deed312349e5ed551eb83d784e3634718a
Radu-A <elcastravete@gmail.com>
2023-05-15 16:48:04 +0200

    styled/button

--- index.js
+++ index.js
@@ -14,20 +14,39 @@ myform.addEventListener("submit", function(event) {
     const fieldsets = document.getElementsByTagName("fieldset");
     //Sabemos también el número fildsets cubiertos
     const checked = document.querySelectorAll("input:checked");
+    //si hay menos fieldsets cubiertos que preguntas muestra alerta
     if (checked.length < fieldsets.length) {
         alert("Debes responder a todas las preguntas")
+        //si todos los fildsets han sido cubiertos continúa
     } else {
+        //recorro todos los fielsets
         for (let i = 0; i < fieldsets.length; i++) {
+            //almaceno el input seleccionado
             const answer = document.querySelector(`#fieldset${i + 1} input:checked`);
+            //almaceno el div padre
             const answerNodes = document.querySelector(`#fieldset${i + 1} input:checked + label`);
-            console.log(answer);
+            //almaceno el div donde está legend para luego añadir el mensaje
+            const legend = document.querySelector(`#fieldset${i + 1} .dlegend`)
+            //creo el mensaje vacío
+            const message = document.createElement("h5");
             if (answer.id === correct[i]) {
+                //desmarco la opción
                 answer.checked = false;
+                //añado la clase "correct" al div padre
                 answerNodes.classList.add("correct");
-                console.log(answerNodes);
+                //añado contenido al mensaje
+                message.innerText = "La respuesta es correcta";
+                message.classList.add("correct-message");
+                legend.appendChild(message);
             } else {
+                //desmarco la opción
                 answer.checked = false;
+                //añado la clase wrong al div padre
                 answerNodes.classList.add("wrong");
+                //añado contenido al mensaje
+                message.innerText = "La respuesta no es correcta";
+                message.classList.add("wrong-message");
+                legend.appendChild(message);
             }
         }
     }
