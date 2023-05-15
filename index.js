b503bc1122bd265888dfb31e80c352b104ada066
Radu-A <elcastravete@gmail.com>
2023-05-15 13:08:55 +0200

    validation/styles

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+//VARIABLES
+//creo "myform" para luego usar el evento "submit"
 const myform = document.getElementById("myform");
-const correct = ['cocodrilo', 'windsor'];
+//array con las respuestas correctas
+const correct = ['cocodrilo', 'windsor', 'seis', 'clon', 'urano', 'amazonas', 'freud', '969', 'd', 'gotico'];
+const inputs = document.querySelectorAll("input");
 
+
+//VALIDAR RESPUESTAS
 myform.addEventListener("submit", function(event) {
     event.preventDefault();
     //Primero nos aseguramos de que responda todas las preguntas
@@ -12,13 +18,15 @@ myform.addEventListener("submit", function(event) {
         alert("Debes responder a todas las preguntas")
     } else {
         for (let i = 0; i < fieldsets.length; i++) {
-            let answer = document.querySelector(`#fieldset${i + 1} input:checked`);
-            let answerNodes = document.querySelector(`#fieldset${i + 1} input:checked + label`);
+            const answer = document.querySelector(`#fieldset${i + 1} input:checked`);
+            const answerNodes = document.querySelector(`#fieldset${i + 1} input:checked + label`);
             console.log(answer);
             if (answer.id === correct[i]) {
+                answer.checked = false;
                 answerNodes.classList.add("correct");
                 console.log(answerNodes);
             } else {
+                answer.checked = false;
                 answerNodes.classList.add("wrong");
             }
         }
